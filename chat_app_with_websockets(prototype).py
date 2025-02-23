bf0450f2786bb96d40af3c223f0172068d73f9fa
Radowan30 <radowanahmed30@gmail.com>
2025-02-24 01:34:37 +0800

    working search and simple manim video generation

--- chat_app_with_websockets(prototype).py
+++ chat_app_with_websockets(prototype).py
@@ -1,5 +1,7 @@
 from __future__ import annotations as _annotations
+import fastapi
 from fastapi import FastAPI, WebSocket
+from fastapi import Depends, Request
 from fastapi.responses import HTMLResponse
 import random
 import logfire
@@ -8,12 +10,33 @@ import os
 from dataclasses import dataclass
 from datetime import datetime
 from typing import Any
-from devtools import debug
 from httpx import AsyncClient
 from dotenv import load_dotenv
 from openai import AsyncOpenAI
 from pydantic_ai.models.openai import OpenAIModel
 from pydantic_ai import Agent, ModelRetry, RunContext
+import sqlite3
+from concurrent.futures.thread import ThreadPoolExecutor
+from contextlib import asynccontextmanager
+from pathlib import Path
+from collections.abc import AsyncIterator
+from typing import Annotated, Any, Callable, Literal, TypeVar
+from typing_extensions import LiteralString, ParamSpec, TypedDict
+from functools import partial
+from pydantic_ai.messages import (
+    ModelMessage,
+    ModelMessagesTypeAdapter,
+    ModelRequest,
+    ModelResponse,
+    TextPart,
+    UserPromptPart,
+)
+from fastapi.logger import logger as fastapi_logger
+from pydantic import BaseModel, Field
+from pydantic_ai.messages import ModelRequest
+from typing import List
+
+
 
 load_dotenv()
 llm = os.getenv('LLM_MODEL', 'gpt-4o')
@@ -21,7 +44,14 @@ llm = os.getenv('LLM_MODEL', 'gpt-4o')
 model = OpenAIModel(llm)
 
 # 'if-token-present' means nothing will be sent (and the example will work) if you don't have logfire configured
-logfire.configure(send_to_logfire='if-token-present')
+# logfire.configure(send_to_logfire='if-token-present')
+
+logfire.configure(
+    service_name="chatbot"    
+)
+
+
+
 
 @dataclass
 class Deps:
@@ -30,7 +60,7 @@ class Deps:
 
 web_search_agent = Agent(
     model,
-    system_prompt=f'You are an expert at researching the web to answer user questions. The current date is: {datetime.now().strftime("%Y-%m-%d")}',
+    system_prompt=f'You are a helpful assistant, and when needed you are also an expert at researching the web to answer user questions. The current date is: {datetime.now().strftime("%Y-%m-%d")}',
     deps_type=Deps,
     retries=2
 )
@@ -85,9 +115,21 @@ async def search_web(
     return "\n".join(results) if results else "No results found for the query."
 
 
-app = FastAPI()
 
-logfire.configure(service_name="chatbot")
+
+agent = Agent('openai:gpt-4o')
+THIS_DIR = Path(__file__).parent
+
+
+@asynccontextmanager
+async def lifespan(_app: fastapi.FastAPI):
+    async with Database.connect() as db:
+        yield {'db': db}
+
+
+app = fastapi.FastAPI(lifespan=lifespan)
+
+
 logfire.instrument_fastapi(app)
 
 html = """
@@ -140,41 +182,106 @@ async def get():
     return html
 
 
-# agent = Agent(  
-#     'openai:gpt-4o-mini',
-#     system_prompt='You are an open source developer, and you did a lot of stuff yesterday.',  
-# )
+async def get_db(request: Request) -> Database:
+    return request.app.state.db
 
-# @agent.tool_plain
-# def what_i_did_yesterday():
-#     stuff_done = [
-#         "I wrote a new blog post",
-#         "I fixed a bug in my project",
-#         "I learned a new programming language",
-#         "I released a new version of my project",
-#     ]
-#     return random.choice(stuff_done)
+
 
 @app.websocket("/ws")
-async def websocket_endpoint(websocket: WebSocket):
+async def websocket_endpoint(websocket: WebSocket, database: Database = Depends(get_db)):
     await websocket.accept()
+
     async with AsyncClient() as client:
         brave_api_key = os.getenv('BRAVE_API_KEY', None)
         deps = Deps(client=client, brave_api_key=brave_api_key)
         async for data in websocket.iter_text():
-            result = await web_search_agent.run(user_prompt=data, message_history=[], deps=deps)
+            # get the chat history so far to pass as context to the agent
+            messages = await database.get_messages()
+
+            result = await web_search_agent.run(user_prompt=data, message_history=messages, deps=deps)                
+
+            # add new messages (e.g. the user prompt and the agent response in this case) to the database
+            await database.add_messages(result.new_messages_json())
+            
             await websocket.send_text(result.data)
-
-
-# async def main():
-#     async with AsyncClient() as client:
-#         brave_api_key = os.getenv('BRAVE_API_KEY', None)
-#         deps = Deps(client=client, brave_api_key=brave_api_key)
-
-#         result = await web_search_agent.run(
-#             'Give me some articles talking about the new release of React 19.', deps=deps
-#         )
+                
         
-#         debug(result)
-#         print('Response:', result.data)
 
+
+P = ParamSpec('P')
+R = TypeVar('R')
+#Dataclass for the database
+@dataclass
+class Database:
+    """Rudimentary database to store chat messages in SQLite.
+
+    The SQLite standard library package is synchronous, so we
+    use a thread pool executor to run queries asynchronously.
+    """
+
+    con: sqlite3.Connection
+    _loop: asyncio.AbstractEventLoop
+    _executor: ThreadPoolExecutor
+
+    @classmethod
+    @asynccontextmanager
+    async def connect(
+        cls, file: Path = THIS_DIR / '.chat_app_messages.sqlite'
+    ) -> AsyncIterator[Database]:
+        with logfire.span('connect to DB'):
+            loop = asyncio.get_event_loop()
+            executor = ThreadPoolExecutor(max_workers=1)
+            con = await loop.run_in_executor(executor, cls._connect, file)
+            slf = cls(con, loop, executor)
+        try:
+            yield slf
+        finally:
+            await slf._asyncify(con.close)
+
+    @staticmethod
+    def _connect(file: Path) -> sqlite3.Connection:
+        con = sqlite3.connect(str(file), check_same_thread=False)
+        con = logfire.instrument_sqlite3(con)
+        cur = con.cursor()
+        cur.execute(
+            'CREATE TABLE IF NOT EXISTS messages (id INT PRIMARY KEY, message_list TEXT);'
+        )
+        con.commit()
+        return con
+
+    async def add_messages(self, messages: bytes):
+        await self._asyncify(
+            self._execute,
+            'INSERT INTO messages (message_list) VALUES (?);',
+            messages,
+            commit=True,
+        )
+        await self._asyncify(self.con.commit)
+
+    async def get_messages(self) -> list[ModelMessage]:
+        c = await self._asyncify(
+            self._execute, 'SELECT message_list FROM messages order by id'
+        )
+        rows = await self._asyncify(c.fetchall)
+        messages: list[ModelMessage] = []
+        for row in rows:
+            messages.extend(ModelMessagesTypeAdapter.validate_json(row[0]))
+        return messages
+
+    def _execute(
+        self, sql: LiteralString, *args: Any, commit: bool = False
+    ) -> sqlite3.Cursor:
+        cur = self.con.cursor()
+        cur.execute(sql, args)
+        if commit:
+            self.con.commit()
+        return cur
+
+    async def _asyncify(
+        self, func: Callable[P, R], *args: P.args, **kwargs: P.kwargs
+    ) -> R:
+        return await self._loop.run_in_executor(  # type: ignore
+            self._executor,
+            partial(func, **kwargs),
+            *args,  # type: ignore
+        )
